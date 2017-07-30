ddbfc5407464a3232c74523c44ba3db095b2ceae
TianYouH <1406957473@qq.com>
2017-07-30 22:08:47 +0800

    let 和 const 完结

--- let 和 const 命令/main.js
+++ let 和 const 命令/main.js
@@ -1,3 +1,9 @@
+// ⭐es6 的声明变量的6种方法
+// ES5 只有两种声明变量的方法：var命令和function命令。
+// ES6除了添加let和const命令，后面章节还会提到，
+// 另外两种声明变量的方法：import命令和class命令。
+// 所以，ES6 一共有6种声明变量的方法。
+
 // 基础用法
 
 // 测试一
@@ -268,6 +274,8 @@
 // if (true)
 //   function f() {}
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 // const
 // 基本用法
 
@@ -301,3 +309,119 @@
 // const message = "Goodbye!";
 // const age = 30;
 
+// 本质 ：const实际上保证的，并不是变量的值不得改动，而是变量指向的那个内存地址不得改动
+
+// 测试一
+// {
+//   const foo = {};
+//   foo.a = 123;
+//   console.log(foo.a);
+//   foo = {}; //Assignment to constant variable.  Assignment to constant variable.
+// }
+
+// 测试二
+// const a = [];
+// a.push('nihao');
+// a.length = 0;
+// a.push('nihao');
+// console.log(a.length)
+// a = ['tianTian']   //报错
+
+// 测试三 ： 冻结对象
+'use strict'
+// const foo = Object.freeze({});
+// 常规模式时，下面一行不起作用；
+// 严格模式时，该行会报错
+// foo.a = 123;
+
+// 测试四 ：冻结对象及属性
+// var hhhh = (obj) => {
+//   Object.freeze(obj);
+//   console.log(Object.keys(obj))
+//   Object.keys(obj).forEach((key, i) => {
+//     if (typeof obj[key] == 'object') {
+//       hhhh(obj[key]);
+//     }
+//   })
+// }
+
+// // 只能冻结const对象
+// const huang = {
+//   name: '黄金亮',
+//   age: 12,
+//   height: 1.67
+// }
+// hhhh(huang);
+// huang.name = 12; // 错误
+// console.log(huang)
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//顶层对象的属性  浏览器环境window    Node环境global
+// 测试一 : ES5之中，顶层对象的属性与全局变量是等价的.
+// window.a = 1
+// console.log(a);
+// global.b = 1;
+// console.log(b)
+// var c = 3;  //node环境测试，貌似不等价
+// console.log(global.c) //undefind
+// console.log(c) //3
+
+// 测试二 ： var命令和function命令声明的全局变量，依旧是顶层对象的属性；
+          // 另一方面规定，let命令、const命令、class命令声明的全局变量，不属于顶层对象的属性。
+          // 也就是说，从ES6开始，全局变量将逐步与顶层对象的属性脱钩。
+// var a = 1;
+// // 如果在Node的REPL环境，可以写成global.a
+// // 或者采用通用方法，写成this.a
+// window.a // 1
+
+// let b = 1;
+// window.b // undefined
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// global 对象  ⭐⭐
+// ES5 的顶层对象，本身也是一个问题，因为它在各种实现里面是不统一的。
+
+// 浏览器里面，顶层对象是window，但 Node 和 Web Worker 没有window。
+// 浏览器和 Web Worker 里面，self也指向顶层对象，但是 Node 没有self。
+// Node 里面，顶层对象是global，但其他环境都不支持。
+// 同一段代码为了能够在各种环境，都能取到顶层对象，现在一般是使用this变量，但是有局限性。
+
+// 全局环境中，this会返回顶层对象。但是，Node 模块和 ES6 模块中，this返回的是当前模块。
+// 函数里面的this，如果函数不是作为对象的方法运行，而是单纯作为函数运行，this会指向顶层对象。但是，严格模式下，这时this会返回undefined。
+// 不管是严格模式，还是普通模式，new Function('return this')()，总是会返回全局对象。但是，如果浏览器用了CSP（Content Security Policy，内容安全政策），那么eval、new Function这些方法都可能无法使用。
+// 综上所述，很难找到一种方法，可以在所有情况下，都取到顶层对象。下面是两种勉强可以使用的方法。
+
+// 方法一
+// (typeof window !== 'undefined'
+//    ? window
+//    : (typeof process === 'object' &&
+//       typeof require === 'function' &&
+//       typeof global === 'object')
+//      ? global
+//      : this);
+
+// 方法二
+// var getGlobal = function () {
+//   if (typeof self !== 'undefined') { return self; }
+//   if (typeof window !== 'undefined') { return window; }
+//   if (typeof global !== 'undefined') { return global; }
+//   throw new Error('unable to locate global object');
+// };
+
+
+// 现在有一个提案，在语言标准的层面，引入global作为顶层对象。也就是说，在所有环境下，global都是存在的，都可以从它拿到顶层对象。
+
+// 垫片库system.global模拟了这个提案，可以在所有环境拿到global。
+// CommonJS 的写法
+// require('system.global/shim')();
+// ES6 模块的写法
+// import shim from 'system.global/shim'; shim();
+// 上面代码可以保证各种环境里面，global对象都是存在的。
+
+// CommonJS 的写法
+// var global = require('system.global')();
+// ES6 模块的写法
+// import getGlobal from 'system.global';
+// const global = getGlobal();
+// 上面代码将顶层对象放入变量global。
