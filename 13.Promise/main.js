f2e0c9da13143882fdd0bf754f854d0cefda57e8
JINSHANSHAN\LiTian <1406957473@qq.com>
2018-01-04 14:42:35 +0800

    Es6Promise学习

--- 13.Promise/main.js
+++ 13.Promise/main.js
@@ -199,7 +199,48 @@ const promiseError = new Promise((resolve, reject) => {
   //   reject => {
   //     console.log('失败', reject)
   //   }
+  // ).catch(
+  //   error => {
+  //     console.log('错误被catch捕获到', error)
+  //   }
   // )
 }
 
 // Promise 对象的错误具有“冒泡”性质，会一直向后传递，直到被捕获为止。也就是说，错误总是会被下一个catch语句捕获。
+// 跟传统的try/catch代码块不同的是，如果没有使用catch方法指定错误处理的回调函数，Promise 对象抛出的错误不会传递到外层代码，即不会有任何反应。
+
+
+// Promise 内部的错误不会影响到 Promise 外部的代码
+{
+  // const someAsyncThing = function() {
+  //   return new Promise(function(resolve, reject) {
+  //     // 下面一行会报错，因为x没有声明
+  //     resolve(x + 2);
+  //   });
+  // };
+  
+  // someAsyncThing().then(function() {
+  //   console.log('everything is great');
+  // });
+  
+  // setTimeout(() => { console.log(123) }, 2000);
+  // // Uncaught (in promise) ReferenceError: x is not defined
+  // // 123
+}
+// 上面代码中，someAsyncThing函数产生的 Promise 对象，内部有语法错误。
+// 浏览器运行到这一行，会打印出错误提示ReferenceError: x is not defined，但是不会退出进程、终止脚本执行，2 秒之后还是会输出123。
+// 这就是说，Promise 内部的错误不会影响到 Promise 外部的代码，通俗的说法就是“Promise 会吃掉错误”。
+
+
+{
+  const promise = new Promise(function (resolve, reject) {
+    resolve('ok');
+    setTimeout(function () { throw new Error('test'); console.log('123456') }, 0)
+  });
+  promise.then(function (value) { console.log(value) });
+  // ok
+  // Uncaught Error: test
+}
+// Promise 指定在下一轮“事件循环”再抛出错误。
+// 到了那个时候，Promise 的运行已经结束了，所以这个错误是在 Promise 函数体外抛出的，会冒泡到最外层，成了未捕获的错误。
+
