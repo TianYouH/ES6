4082667e3db62b402f691b4a9fb147fd3861e691
huangjinliang <1406957473@qq.com>
2018-05-11 18:01:31 +0800

    破·

--- 11.Proxy/mian.js
+++ 11.Proxy/mian.js
@@ -35,3 +35,35 @@ buttonSubtract.addEventListener('click', function () {
 })
 */
 
+
+// 一个拦截器函数，可以设置拦截多个操作。
+// 对于可以设置、但没有设置拦截的操作，则直接落在目标对象上，按照原先的方式产生结果。
+// 测试例子2
+/*
+var handler = {
+  get: function(target, name) {
+    if (name === 'prototype') {
+      return Object.prototype;
+    }
+    return 'Hello, ' + name;
+  },
+
+  apply: function(target, thisBinding, args) {
+    return args[0];
+  },
+
+  construct: function(target, args) {
+    return {value: args[1]};
+  }
+};
+
+var fproxy = new Proxy(function(x, y) {
+  return x + y;
+}, handler);
+
+console.log(fproxy(1, 2))  // 1
+console.log(new fproxy(1, 2))  // {value: 2}
+console.log(fproxy.prototype === Object.prototype)  // true
+console.log(fproxy.foo === "Hello, foo")  // true
+*/
+
